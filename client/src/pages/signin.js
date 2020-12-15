0972381b4ee56825ee3d7f99c0faf8b1ffdf888e
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-12-14 21:33:10 -0600

    created a input form

--- client/src/pages/signin.js
+++ client/src/pages/signin.js
@@ -3,20 +3,21 @@ import Button from "../components/Button/button"
 //import { Link } from "react-router-dom";
 //import { Col, Row, Container } from "../components/Grid";
 //import { List, ListItem } from "../components/List";
-//import { Input, TextArea, FormBtn } from "../components/Form";
+import Input from "../components/Input/input";
 
 function SignIn(){
 
     return(
         <div>
+            <Input name = "Username">
+            </Input>
+            <Input name = "Password">
+            </Input>
             <Button  
             name = "Sign In">
-            
             </Button>
-            <Button  
-            name = "Sign Out">
+
             
-            </Button>
         </div>
     );
 }
