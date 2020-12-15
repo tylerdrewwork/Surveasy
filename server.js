f940a0fedfed8daee690fc844e45b081ef67f4a4
nbur4556 <nbur4556@gmail.com>
2020-12-14 20:56:04 -0600

    feat: create user model for user credentials

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 
 // Define Middleware
-if(process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static('./client/build'));
 }
 
-// Connet to the Mongo DB
+// Connect to the Mongo DB
 mongoose.connect(
     // Use MONGODB URI from environment, otherwise use local database
     process.env.MONGODB_URI || "mongodb://localhost/surveydb",
