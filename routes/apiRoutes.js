a5c65b223213a10f96c00a8bccdea79035451730
Sakiskid <tylerdrew.home@gmail.com>
2020-12-14 22:51:25 -0600

    feat: added more api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,20 @@
 const router = require('express').Router();
 const db = require('models');
-const userController = require('../controllers/userController');
+const UserController = require('../controllers/UserController');
+
+// Create a new UserController 
+const userController = new UserController;
 
 // USER Routes
 
 // When hitting the /api/user route, reference the controller and run the respective function
 router.route("/api/user")
-    // .get(userController.read) 
+    .get(userController.getAllUsers) 
     .post(userController.createUser);
 
 router.route("/api/user/:id")
-    // .get()
-    // .put()
+    .get(userController.getUserById)
+    // .put(userController.)
     // .delete()
 
 // SURVEY Routes
