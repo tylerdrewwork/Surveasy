eaa3fd41815a3fc5c91b4cd32536b57b43c2ce43
JCJ <jesse.jackson.c@gmail.com>
2020-12-15 20:33:02 -0600

    removing unused imports

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Button from "../components/Button/button";
-//import { Link } from "react-router-dom";
-//import { Col, Row, Container } from "../components/Grid";
-//import { List, ListItem } from "../components/List";
 import Input from "../components/Input/input";
 import { useHistory } from "react-router-dom";
 
