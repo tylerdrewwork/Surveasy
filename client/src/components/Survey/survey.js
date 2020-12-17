63e8da4bf3eee658b45ca6b01746bded64f1dd18
JCJ <jesse.jackson.c@gmail.com>
2020-12-17 00:35:40 -0600

    Survey component that utilizes question and answer imports

---
+++ client/src/components/Survey/survey.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./survey.css";
+import Answer from "../Answer/answer";
+import Question from "../Question/question";
+
+function Survey(props) {
+  return (
+    <div className="back-div" {...props}>
+      {Question}
+      {Answer}
+      {Answer}
+      {Answer}
+    </div>
+  );
+}
+
+export default Survey;
