1fb17e0ef33e46739cee9a67d3143860b0382021
nbur4556 <nbur4556@gmail.com>
2020-12-16 20:05:56 -0600

    feat: create survey model

--- models/Survey.js
+++ models/Survey.js
@@ -1,28 +1,20 @@
-// mock model for survey
-const survey = {
-    title: "Food Survey",
-    active: true,
-    public: true,
-    questions: [
-        {
-            id: 0,
-            question: "What is your favorite ice cream",
-            choices: [
-                {id: 1, name: "Rocky Road", votes: 5},
-                {id: 2, name: "Butter Pecan", votes: 0},
-                {id: 3, name: "Mint :(", votes: 0},
-                {id: 4, name: "French Vanilla", votes: 0}
-            ],
-        },
-        {
-            id: 1,
-            question: "What is your favorite italian food",
-            choices: [
-                {name: "Rocky Road", votes: 5},
-                {name: "Butter Pecan", votes: 0},
-                {name: "Mint :(", votes: 0},
-                {name: "French Vanilla", votes: 0}
-            ],
-        }
-    ],   
-}
+const mongoose = require('mongoose');
+
+const SurveySchema = mongoose.Schema({
+    title: String,
+    active: Boolean,
+    public: Boolean,
+    questions: [{
+        questionId: String,
+        question: String,
+        choices: [{
+            choiceId: String,
+            choice: String,
+            votes: Number
+        }]
+    }]
+});
+
+const Survey = mongoose.model('Survey', SurveySchema);
+
+module.exports = Survey;
