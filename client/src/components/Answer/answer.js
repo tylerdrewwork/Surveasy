0991e57f2cb47524cf4f0df31804c098c69b5a2a
JCJ <jesse.jackson.c@gmail.com>
2020-12-16 22:42:00 -0600

    Answer component

---
+++ client/src/components/Answer/answer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./answer";
+
+function Answer(answer) {
+    return (
+        <div onClick="#">
+            { answer }
+        </div>
+    )
+};
+
+export default Answer;
