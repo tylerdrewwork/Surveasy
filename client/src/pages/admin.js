f98ee141cbe3d71c1a9e82fedff397c4537bc72b
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-12-17 18:02:05 -0600

    starting to work on admin page

--- client/src/pages/admin.js
+++ client/src/pages/admin.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 import Button from "../components/Button/button";
 //import { Link } from "react-router-dom";
-//import { Col, Row, Container } from "../components/Grid";
-//import { List, ListItem } from "../components/List";
+import {Grid, Row, Col} from "react-bootstrap"
 import Input from "../components/Input/input";
 import "./style.css";
 
 function Admin() {
   return (
+    //navbar will be set up here 
+    // ---- Have the log out button link here 
+    //starting the side survey page 
     <div>
-      <h1>Welcom</h1>
+      <h1>Welcome</h1>
     </div>
   );
 }
