b14fadb0eb9a49ab36b03643db459a44efda44b2
Sakiskid <tylerdrew.home@gmail.com>
2020-12-17 20:14:41 -0600

    feat: added blank methods for surveyController

---
+++ controllers/surveyController.js
@@ -0,0 +1,64 @@
+const db = require('../models');
+
+function SurveyController() {}
+
+//
+//  CREATE METHODS
+//
+
+SurveyController.prototype.createSurvey = function () {
+
+}
+
+SurveyController.prototype.createQuestion = function () {
+
+}
+
+SurveyController.prototype.createChoice = function () {
+    
+}
+
+// 
+//  READ METHODS
+// 
+
+SurveyController.prototype.getSurveyById = function () {
+
+}
+
+// May be redundant: SurveyController.prototype.getQuestionById = function () {}
+
+// 
+//  UPDATE METHODS
+// 
+
+/**
+ * @description Updates the survey name, description, etc. Does NOT update question info
+ */
+SurveyController.prototype.updateSurveyInfo = function () {
+
+}
+
+SurveyController.prototype.updateQuestionTitle = function () {
+
+}
+
+SurveyController.prototype.updateChoice = function () {
+    
+}
+
+// 
+//  DELETE METHODS
+// 
+
+SurveyController.prototype.deleteSurvey = function () {
+
+}
+
+SurveyController.prototype.deleteQuestion = function () {
+
+}
+
+SurveyController.prototype.deleteChoice = function () {
+
+}
