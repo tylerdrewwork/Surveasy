3a8b425505ffaa72583aeb83bc8e668ee41aa4e8
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-12-17 19:11:22 -0600

    adding in the survey function 4

--- client/src/pages/admin.js
+++ client/src/pages/admin.js
@@ -3,64 +3,27 @@ import Button from "../components/Button/button";
 //import { Link } from "react-router-dom";
 
 import Input from "../components/Input/input";
-import "./style.css";
-import {Grid, Row, Col} from "react-bootstrap"
-import SurveyList from "../components/SurveyList/surveyList"
 
+import SurveyList from "../components/SurveyList/surveyList"
+import {Container, Grid, Row, Col} from "react-bootstrap";
 function Admin() {
   return (
     //navbar will be set up here 
     // ---- Have the log out button link here 
     //starting the side survey page 
     <div>
-      <Row md ={5} float="center">
-          <Col sx={3} md ={3}>
-              
-                  <SurveyList
-                  name = "Ice Cream Survey"
-                  >
-                  </SurveyList>
-
-                  <SurveyList
-                  name = "Italian Food Survey"
-                  >
-                  </SurveyList>
-
-                  <SurveyList
-                  name = "Mexican Food Survey"
-                  >
-                  </SurveyList>
-
-                  <SurveyList
-                  name = "New Survey"
-                  >
-                  </SurveyList>
-              
-          </Col>
-          <Col sx={3} md={3}>
-             
-                  <SurveyList
-                  name = "Ice Cream Survey"
-                  >
-                  </SurveyList>
-
-                  <SurveyList
-                  name = "Italian Food Survey"
-                  >
-                  </SurveyList>
-
-                  <SurveyList
-                  name = "Mexican Food Survey"
-                  >
-                  </SurveyList>
-
-                  <SurveyList
-                  name = "New Survey"
-                  >
-                  </SurveyList>
-              
-          </Col>
+      <Container>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col xs={6}>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
       </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col xs={5}>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
 
     </div>
   );
