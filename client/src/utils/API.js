7a6ce2e87a9dc56817b03de2c309f34b337a7b07
JCJ <jesse.jackson.c@gmail.com>
2020-12-21 18:25:27 -0600

    added comments and established authorization post route

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -25,7 +25,10 @@ export default {
 
   // AUTHORIZATION Routes
   getAuthToken: function (username, password) {
-    return axios.post("/api/auth/")
+    return axios.post("/api/auth/", {
+      username: username,
+      password: password
+    })
   },
 
   // SURVEY Routes
