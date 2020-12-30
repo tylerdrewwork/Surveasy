0b875fbfdeee08d05ca974c7c708d8253b725d2e
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-12-29 20:10:33 -0600

    adding in the chart js data mic

--- client/src/pages/admin.js
+++ client/src/pages/admin.js
@@ -16,7 +16,24 @@ import {Container, Grid, Row, Col} from "react-bootstrap";
 function Admin() {
   const [survey, setSurvey] = useState({});
   const [curSurvey, setCurSurvey] = useState({});
-  
+  const data = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  datasets: [
+    {
+      label: "First dataset",
+      data: [33, 53, 85, 41, 44, 65],
+      fill: true,
+      backgroundColor: "rgba(75,192,192,0.2)",
+      borderColor: "rgba(75,192,192,1)"
+    },
+    {
+      label: "Second dataset",
+      data: [33, 25, 35, 51, 54, 76],
+      fill: false,
+      borderColor: "#742774"
+    }
+  ]
+};
   useEffect(() => {
     uploadSurveys()
     console.log(survey);
