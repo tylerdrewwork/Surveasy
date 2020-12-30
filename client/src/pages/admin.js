6e35ba9a1faa2602d7e67d4101043158f71dc81b
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-12-29 20:10:13 -0600

    adding in the chart js stuff 1

--- client/src/pages/admin.js
+++ client/src/pages/admin.js
@@ -6,7 +6,7 @@ import Input from "../components/Input/input";
 import "./style.css";
 import CreateSurvey from "../components/createSurvey/createSurvey";
 import Navigation from "../components/NavBar/navbar";
-
+import { Line } from "react-chartjs-2";
 
 
 
