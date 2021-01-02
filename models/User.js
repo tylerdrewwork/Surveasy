7548783d28efdd10a2ee5a1bfadb8e639cdd0636
nbur4556 <nbur4556@gmail.com>
2021-01-02 12:30:50 -0600

    fix: require all necessary user credentials on user creation

--- models/User.js
+++ models/User.js
@@ -3,13 +3,21 @@ const mongoose = require('mongoose');
 const UserSchema = mongoose.Schema({
     username: {
         type: String,
-        unique: true
+        unique: true,
+        required: true
     },
-    passToken: String,
-    salt: String,
+    passToken: {
+        type: String,
+        required: true
+    },
+    salt: {
+        type: String,
+        required: true
+    },
     email: {
         type: String,
-        unique: true
+        unique: true,
+        required: true
     },
     surveys: [{
         type: mongoose.Schema.Types.ObjectId,
