423523b78c416c0180d29dc9e12de26c859828ad
nbur4556 <nbur4556@gmail.com>
2021-01-04 19:21:10 -0600

    fix: retrive jwt token in create survey component

--- client/src/components/createSurvey/createSurvey.js
+++ client/src/components/createSurvey/createSurvey.js
@@ -7,18 +7,22 @@ import API from "../../utils/API";
 
 function CreateSurvey() {
 
-    const [surveyData, setSurveyData] = useState({})
+  const [surveyData, setSurveyData] = useState({})
 
-    function handleInputChange(event) {
-        const { name, value } = event.target;
-        setSurveyData({...surveyData, [name]: value});
-    } 
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setSurveyData({ ...surveyData, [name]: value });
+  }
 
-    function handleFormSubmit(event) {
-        event.preventDefault();
-        API.createSurvey();
-        console.log("Testing handle form submit data: " + JSON.stringify(surveyData))
-    }
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const token = localStorage.getItem('token');
+
+    console.log(token);
+
+
+    // API.createSurvey()
+  }
 
   return (
     <div className="back-div">
