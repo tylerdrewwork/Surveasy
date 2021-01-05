3d17b839667a0128e6d1ccf282c4b20286885365
nbur4556 <nbur4556@gmail.com>
2021-01-04 20:25:41 -0600

    fix: separate inputs in create survey state

--- client/src/components/createSurvey/createSurvey.js
+++ client/src/components/createSurvey/createSurvey.js
@@ -7,23 +7,33 @@ import API from "../../utils/API";
 
 function CreateSurvey() {
 
-  const [surveyData, setSurveyData] = useState({})
+  const [questionInputs, setQuestionInputs] = useState(["", ""]);
+  const [surveyData, setSurveyData] = useState(new Array(questionInputs.length));
+
 
   function handleInputChange(event) {
-    const { name, value } = event.target;
-    setSurveyData({ ...surveyData, [name]: value });
+    const { name, value, parentElement } = event.target;
+    const key = parentElement.getAttribute('data-key');
+    const surveyDataArray = surveyData;
+
+    surveyDataArray[key] = { ...surveyDataArray[key], [name]: value };
+    setSurveyData({ ...surveyData, surveyDataArray });
   }
 
   function handleFormSubmit(event) {
     event.preventDefault();
     const token = localStorage.getItem('token');
 
-    API.createSurvey(formatSurveyData(surveyData), token).then(result => {
-      console.log(result);
-    });
+    console.log(surveyData);
+
+    // API.createSurvey(formatSurveyData(surveyData), token).then(result => {
+    //   console.log(result);
+    // });
   }
 
   function formatSurveyData(data) {
+
+
     const formattedData = {
       title: data.SurveyName,
       questions: [
@@ -45,11 +55,25 @@ function CreateSurvey() {
   return (
     <div className="back-div">
       <Input onChange={handleInputChange} name="SurveyName" />
-      <Input onChange={handleInputChange} name="QuestionTitle" />
-      <Input onChange={handleInputChange} name="Option1" />
-      <Input onChange={handleInputChange} name="Option2" />
-      <Input onChange={handleInputChange} name="Option3" />
-      <Input onChange={handleInputChange} name="Option4" />
+
+      {questionInputs.map((input, index) => {
+        return <div data-key={index} key={index}>
+          <Input onChange={handleInputChange} name={`QuestionName`} />
+          <Input onChange={handleInputChange} name={`Option1`} />
+          <Input onChange={handleInputChange} name={`Option2`} />
+          <Input onChange={handleInputChange} name={`Option3`} />
+          <Input onChange={handleInputChange} name={`Option4`} />
+        </div>
+      })}
+
+      {/* <div>
+        <Input onChange={handleInputChange} name="QuestionTitle" />
+        <Input onChange={handleInputChange} name="Option1" />
+        <Input onChange={handleInputChange} name="Option2" />
+        <Input onChange={handleInputChange} name="Option3" />
+        <Input onChange={handleInputChange} name="Option4" />
+      </div> */}
+
       <Button onClick={handleFormSubmit} name="Submit" />
     </div>
   );
