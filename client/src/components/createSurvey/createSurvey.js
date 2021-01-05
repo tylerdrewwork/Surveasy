0a1e36098854ed67d84ac5b47a403726dea7609a
nbur4556 <nbur4556@gmail.com>
2021-01-04 19:29:39 -0600

    feat: create a survey using the create survey component

--- client/src/components/createSurvey/createSurvey.js
+++ client/src/components/createSurvey/createSurvey.js
@@ -18,20 +18,34 @@ function CreateSurvey() {
     event.preventDefault();
     const token = localStorage.getItem('token');
 
-    console.log(token);
+    const setData = {
+      title: surveyData.SurveyName,
+      questions: [
+        {
+          question: surveyData.QuestionTitle,
+          choices: [
+            { choice: surveyData.Option1 },
+            { choice: surveyData.Option2 },
+            { choice: surveyData.Option3 },
+            { choice: surveyData.Option4 }
+          ]
+        }
+      ]
+    }
 
-
-    // API.createSurvey()
+    API.createSurvey(setData, token).then(result => {
+      console.log(result);
+    });
   }
 
   return (
     <div className="back-div">
       <Input onChange={handleInputChange} name="SurveyName" />
       <Input onChange={handleInputChange} name="QuestionTitle" />
-      <Input onChange={handleInputChange} name="Option" />
-      <Input onChange={handleInputChange} name="Option" />
-      <Input onChange={handleInputChange} name="Option" />
-      <Input onChange={handleInputChange} name="Option" />
+      <Input onChange={handleInputChange} name="Option1" />
+      <Input onChange={handleInputChange} name="Option2" />
+      <Input onChange={handleInputChange} name="Option3" />
+      <Input onChange={handleInputChange} name="Option4" />
       <Button onClick={handleFormSubmit} name="Submit" />
     </div>
   );
