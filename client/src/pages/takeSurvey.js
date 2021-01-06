9d550789306e4d6a495bd646e8c309d43443edf1
JCJ <jesse.jackson.c@gmail.com>
2021-01-05 20:30:46 -0600

    successful render of survey data onto page

--- client/src/pages/takeSurvey.js
+++ client/src/pages/takeSurvey.js
@@ -1,22 +1,38 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/API";
-import { Grid, Row, Col } from "react-bootstrap";
+import { Container, Grid, Row, Col } from "react-bootstrap";
 import "./style.css";
 import NavigationSurvey from "../components/NavBarSurvey/navbarSurvey";
 import { useParams } from "react-router-dom";
+import { get } from "mongoose";
 
 function TakeSurvey() {
-  let { id } = useParams();
+    const [ survey, setSurvey ] = useState({});
+    let { id } = useParams();
 
+    useEffect(() => {
+        getSurveyById();
+    }, [])
+
+  function getSurveyById() {
   API.getUserSurveyById(id)
     .then((res) => {
       console.log(res.data);
+      setSurvey(res.data);
     })
     .catch((err) => console.log(err));
+}
 
   return (
     <div>
       <NavigationSurvey />
+      <Container>
+          <Row>
+              <Col>
+              {survey.title}
+              </Col>
+          </Row>
+      </Container>
     </div>
   );
 }
