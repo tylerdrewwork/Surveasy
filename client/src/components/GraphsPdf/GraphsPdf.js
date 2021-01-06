4d4fbbc6794bf36d4decdce7163daa7e2d1a743a
nbur4556 <nbur4556@gmail.com>
2021-01-05 18:56:50 -0600

    feat: create template for graph pdf
    Uses React-pdf package. Requires an npm install.

---
+++ client/src/components/GraphsPdf/GraphsPdf.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Page, Text, View, Document, Stylesheet } from '@react-pdf/renderer';
+
+const GraphPdf = () => {
+    <Document>
+        <Page>
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        </Page>
+    </Document>
+}
+
+module.exports = GraphPdf;
