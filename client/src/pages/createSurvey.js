8999cef62c86f916edfbd4bf310df70f9c3e1a45
JCJ <jesse.jackson.c@gmail.com>
2021-01-05 19:08:58 -0600

    Putting navigation bar into create survey route

--- client/src/pages/createSurvey.js
+++ client/src/pages/createSurvey.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Button from "../components/Button/button";
-import API from "../utils/API"
+import API from "../utils/API";
 import "./style.css";
 import { Container, Grid, Row, Col } from "react-bootstrap";
-import Create from "../components/createSurvey/createSurvey"
-
+import Create from "../components/createSurvey/createSurvey";
+import NavbarSurvey from "../components/NavBarSurvey/navbarSurvey";
 
 function CreateSurvey() {
-
-    return (
-        <div>
-            <Create></Create>
-        </div>
-    )
+  return (
+    <div>
+      <NavbarSurvey />
+      <Create></Create>
+    </div>
+  );
 }
 
 export default CreateSurvey;
