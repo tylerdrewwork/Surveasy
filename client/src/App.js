0e916f40e3bdb96c419dc0cd4d265c88f0d69dc6
nbur4556 <nbur4556@gmail.com>
2021-01-07 20:36:28 -0600

    feat: create template for initial landing page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Analytics from "./pages/analytics"
 import CreateSurvey from "./pages/createSurvey"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import './pages/style.css';
+
 function App() {
   return (
     <Router>
@@ -29,6 +31,21 @@ function App() {
       <Route exact path="/create">
         <CreateSurvey />
       </Route>
+
+      <section className='back-div'>
+        <h1>SurvEasy</h1>
+
+        <div>Sign in Link</div>
+        <div>Sign Up Link</div>
+
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+          non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        </p>
+      </section>
     </Router>
   );
 }
