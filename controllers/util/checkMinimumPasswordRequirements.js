b41d4012206644f404de16ff13620cce3394d2bc
nbur4556 <nbur4556@gmail.com>
2021-01-09 12:55:38 -0600

    feat: check if password has uppercase, lowercase, and number characters

--- controllers/util/checkMinimumPasswordRequirements.js
+++ controllers/util/checkMinimumPasswordRequirements.js
@@ -1,9 +1,20 @@
 const minLength = 8;
 
 function checkMinimumPasswordRequirements(password) {
-    if (password.length < minLength) {
-        return false
-    }
+    // Check if password meets minimum length
+    if (password.length < minLength) { return false; }
+
+    // Check that password has lower case letter
+    const hasLowercase = password.search(/[a-z]/);
+    if (hasLowercase === -1) { return false; }
+
+    // Check that password has upper case letter
+    const hasUppercase = password.search(/[A-Z]/);
+    if (hasUppercase === -1) { return false; }
+
+    // Check that password has number
+    const hasNumber = password.search(/[0-9]/);
+    if (hasNumber === -1) { return false; }
 
     return true;
 }
