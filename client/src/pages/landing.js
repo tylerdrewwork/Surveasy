b1b00be7e6475d67bb2b2e588e84423f2f4dacb4
nbur4556 <nbur4556@gmail.com>
2021-01-12 16:10:24 -0600

    feat: include simple app description on landing page

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,48 +1,44 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import './landing.css';
-import {gsap} from 'gsap';
+import { gsap } from 'gsap';
 
 function Landing() {
-    useEffect( () => {
-        function beginAnimation () {
+    useEffect(() => {
+        function beginAnimation() {
             let timeline = gsap.timeline();
-            timeline.fromTo(".title", 
-                {scale: 0.8, x: -50, opacity: 0,},
-                {scale: 1, x: 0, opacity: 1, duration: 1})
+            timeline.fromTo(".title",
+                { scale: 0.8, x: 0, opacity: 0, },
+                { scale: 1, x: 0, opacity: 1, duration: 1 })
                 .fromTo("#about",
-                {scale: 0.8, x: -50, opacity: 0,},
-                {scale: 1, x: 0, opacity: 1, duration: 1})
+                    { scale: 0.8, x: 0, opacity: 0, },
+                    { scale: 1, x: 0, opacity: 1, duration: 1 })
 
         }
 
         beginAnimation();
     })
-    
+
 
     return (
         <section className='back-div'>
             <h1 className="title">SurvEasy</h1>
 
-            <section>
-                <Link to='/signin' style={{ margin: '1em' }}>Sign in</Link>
-                <Link to='/signup' style={{ margin: '1em' }}>Sign up</Link>
-            </section>
-
             <p className="description" id="about">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                Providing quality surveys, and detailed analytics.
             </p>
 
+            <section>
+                <Link to='/signin' className="btn-link">Sign in</Link>
+                <Link to='/signup' className="btn-link">Sign up</Link>
+            </section>
+
             <p className="description" id="credits">
-                Created By: 
-                <br/><a href="https://github.com/smundhada">Shubhangi Mundhada</a>
-                <br/><a href="https://github.com/nbur4556">Nick Burt</a>
-                <br/><a href="https://github.com/JesseJ713">Jesse Jackson</a>
-                <br/><a href="https://github.com/Sakiskid">Tyler Smith</a>
+                Created By:
+                <br /><a href="https://github.com/smundhada">Shubhangi Mundhada</a>
+                <br /><a href="https://github.com/nbur4556">Nick Burt</a>
+                <br /><a href="https://github.com/JesseJ713">Jesse Jackson</a>
+                <br /><a href="https://github.com/Sakiskid">Tyler Smith</a>
             </p>
         </section>
     )
