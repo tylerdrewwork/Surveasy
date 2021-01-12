0f22a4a3d35ea7529be103bb2a980f853f1019f8
JCJ <jesse.jackson.c@gmail.com>
2021-01-11 19:58:01 -0600

    Display link correctly to match url of selected survey

--- client/src/pages/admin.js
+++ client/src/pages/admin.js
@@ -7,85 +7,94 @@ import "./style.css";
 import CreateSurvey from "../components/createSurvey/createSurvey";
 import NavigationSurvey from "../components/NavBarSurvey/navbarSurvey";
 import { Line } from "react-chartjs-2";
-import SurveyList from "../components/SurveyList/surveyList"
+import SurveyList from "../components/SurveyList/surveyList";
 import { Container, Grid, Row, Col } from "react-bootstrap";
 
 function Admin() {
-    const [survey, setSurvey] = useState({});
-    const [curSurvey, setCurSurvey] = useState({});
-    let token;
-    let selectedSurvey; 
-    useEffect(() => {
-        uploadSurveys()
-        console.log(token);
-        console.log(survey);
-    }, [])
+  const [survey, setSurvey] = useState({});
+  const [curSurvey, setCurSurvey] = useState({});
+  let token;
+  let selectedSurvey;
+  useEffect(() => {
+    uploadSurveys();
+    console.log(token);
+    console.log(survey);
+  }, []);
 
-    useEffect(() => {
-        console.log(curSurvey);
-    }, [curSurvey])
+  useEffect(() => {
+    console.log(curSurvey);
+  }, [curSurvey]);
 
-    function uploadSurveys() {
-        token = localStorage.getItem(`token`);
+  function uploadSurveys() {
+    token = localStorage.getItem(`token`);
 
-        API.getUserSurveys(token)
-          .then((res) => {
-            setSurvey(res.data);
-            accessSurvey(localStorage.getItem(`currentSurvey`))
-            console.log(res.data);
-          })
-          .catch((err) => console.log(err));
-    };
+    API.getUserSurveys(token)
+      .then((res) => {
+        setSurvey(res.data);
+        accessSurvey(localStorage.getItem(`currentSurvey`));
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  }
 
-    function accessSurvey(id) {
-        selectedSurvey = id; 
-        console.log(selectedSurvey);
-        localStorage.setItem('currentSurvey', id);
-        var r = getIndex(id);
-        setCurSurvey(survey[r]);
-    }
+  function accessSurvey(id) {
+    selectedSurvey = id;
+    console.log(selectedSurvey);
+    localStorage.setItem("currentSurvey", id);
+    var r = getIndex(id);
+    setCurSurvey(survey[r]);
+  }
 
-    function getIndex(id) {
-        return survey.findIndex(obj => obj._id === id);
-      }
+  function getIndex(id) {
+    return survey.findIndex((obj) => obj._id === id);
+  }
 
-    return (
+  return (
+    <div>
+      <NavigationSurvey />
 
-        <div>
-            <NavigationSurvey />
-
-        <Container fluid>
-            <Row float="center">
-            <Col sx={3} md={3}>
-                    <div className="back-div">
-                        {Object.keys(survey).map(key => (
-                            <SurveyList name={survey[key].title} onClick={() => accessSurvey(survey[key]._id)} >
-                            </SurveyList>
-                        ))}
-                    </div>
-                </Col>
+      <Container fluid>
+        <Row float="center">
+          <Col sx={3} md={3}>
+            <div className="back-div">
+              {Object.keys(survey).map((key) => (
+                <SurveyList
+                  name={survey[key].title}
+                  onClick={() => accessSurvey(survey[key]._id)}
+                ></SurveyList>
+              ))}
+            </div>
+          </Col>
+          <Col>
+            <div className="back-div" id="displaySurvey">
+              <h1>{curSurvey.title}</h1>
+              <h3>
+                {curSurvey.active == null
+                  ? ""
+                  : "Active : " + curSurvey.active.toString()}
+              </h3>
+            </div>
             <Col>
-                    <div className="back-div" id="displaySurvey">
-                        <h1>{curSurvey.title}</h1>
-                        <h3>{curSurvey.active == null ? '' : "Active : " + curSurvey.active.toString()}</h3>
-                    </div>
-                    <Col>
-                <div className="back-div" id="displaySurvey">
-                    <NavLink
-                        to={{
-                            pathname: `survey/${curSurvey._id}`,
-                        }}>survey/${curSurvey._id}</NavLink> 
-                    <h3>{curSurvey.active == null ? '' : "Active : " + curSurvey.active.toString()}</h3>
-                </div>
+              <div className="back-div" id="displaySurvey">
+                <NavLink
+                  to={{
+                    pathname: `survey/${curSurvey._id}`,
+                  }}
+                >
+                  https://surveasy.herokuapp.com/survey/${curSurvey._id}
+                </NavLink>
+                <h3>
+                  {curSurvey.active == null
+                    ? ""
+                    : "Active : " + curSurvey.active.toString()}
+                </h3>
+              </div>
             </Col>
-                </Col>
-            </Row>
-        </Container>
-
-        </div>
-
-    );
-
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default Admin;
