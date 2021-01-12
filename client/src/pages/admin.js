d5e7c13bb780f9b0e9297a243b3fc8800af80e8f
Shubhangi Mundhada <shubhimundhada@gmail.com>
2021-01-11 19:41:36 -0600

    adding content to radio button 4

--- client/src/pages/admin.js
+++ client/src/pages/admin.js
@@ -90,6 +90,7 @@ function Admin() {
                         <Input onChange={handleInputChange} name={curSurvey.title}></Input>
                         <h3>Edit Active:</h3>
                         <Radio name={curSurvey.active == null ? '' : "Active"} checked={curSurvey.active.toString() == "true"} ></Radio>
+                        <Radio name={curSurvey.active == null ? '' : "Deactive"} checked={curSurvey.active.toString() == "false"} ></Radio>
                         <Col sx={3} md={12}>
                         <Button name="Submit" onClick={handleFormSubmit}></Button>
                         </Col>
