f5455c3f841940b382c901da4154fa562945900c
Sakiskid <tylerdrew.home@gmail.com>
2021-01-13 00:37:15 -0600

    feat: changed navbar colors to better fit scheme

--- client/src/components/NavBarSurvey/navbarSurvey.js
+++ client/src/components/NavBarSurvey/navbarSurvey.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap/";
-import "./navbarSurvey.css";
+import "./navbar.css";
 
 export default function NavigationSurvey() {
   function handleLogout() {
@@ -9,7 +9,7 @@ export default function NavigationSurvey() {
   }
 
   return (
-    <Navbar className="navStyle" bg="dark" variant="dark">
+    <Navbar className="navStyle" bg="light">
       <Navbar.Brand as={Link} href="#home">
         Surveasy
       </Navbar.Brand>
